4633a0cec211f1702db6289d0a95b837e87081cf
aktarulrahul <contact.aktarul@gmail.com>
2022-01-19 14:08:51 +0600

    api for new todo done

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const pool = require('./db');
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+/* ---------------------- ROUTES ---------------------- */
+app.post('/todos', async (req, res) => {
+  try {
+    // console.log(req.body);
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      'INSERT INTO todo (description) VALUES($1) RETURNING *',
+      [description]
+    );
+    res.json(newTodo);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+/* ---------------------- Create a todo ---------------------- */
+
+/* ---------------------- get all todos ---------------------- */
+
+/* ---------------------- get a todo ---------------------- */
+
+/* ---------------------- update a todo ---------------------- */
+
+/* ---------------------- delete a todo ---------------------- */
 
 app.listen(5000, () => {
   console.log('server has started on port 5000');
